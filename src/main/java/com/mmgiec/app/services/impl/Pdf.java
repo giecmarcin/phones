b0cd88dfafebc75e5e5acd78ab975390a036a28b
Marcin Giec <giecmarcin@outlook.com>
2017-01-18 23:58:43 +0100

    //

--- src/main/java/com/mmgiec/app/services/impl/Pdf.java
+++ src/main/java/com/mmgiec/app/services/impl/Pdf.java
@@ -1,19 +1,10 @@
 package com.mmgiec.app.services.impl;
 
 
-import com.itextpdf.text.Anchor;
-import com.itextpdf.text.BadElementException;
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.Chapter;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
+import com.itextpdf.text.*;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.List;
-import com.itextpdf.text.ListItem;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.Section;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -24,6 +15,7 @@ import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -31,23 +23,26 @@ public class Pdf {
     private java.util.List<PhoneAndQuantityInOrder> phonesInOrder;
     private User customer;
     private String FILE;
+    private BaseFont baseFont;
+    private Font font;
+
 
     public Pdf(java.util.List<PhoneAndQuantityInOrder> phonesInOrder, User customer) {
-        String fileName = LocalDate.now().toString() + "-" + customer.getFirst_name() + " " + customer.getLast_name() + "-" +UUID.randomUUID();
-        this.FILE = System.getProperty("user.dir") + "/" + fileName+".pdf";
+        String fileName = LocalDate.now().toString() + "-" + customer.getFirst_name() + " " + customer.getLast_name() + "-" + UUID.randomUUID();
+        this.FILE = System.getProperty("user.dir") + "/" + fileName + ".pdf";
         this.phonesInOrder = phonesInOrder;
         this.customer = customer;
+
+        try {
+            this.baseFont = BaseFont.createFont("c:/windows/fonts/arial.ttf",
+                    BaseFont.CP1250, BaseFont.EMBEDDED);
+            this.font = new Font(baseFont, 12);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
-    private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
-            Font.BOLD);
-    private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12,
-            Font.NORMAL, BaseColor.RED);
-    private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
-            Font.BOLD);
-    private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
-            Font.BOLD);
 
     public void create() throws FileNotFoundException, DocumentException {
         Document document = new Document();
@@ -59,50 +54,38 @@ public class Pdf {
     }
 
     private void addContent(Document document) throws DocumentException {
-        Anchor anchor = new Anchor("Faktura", catFont);
+        Anchor anchor = new Anchor("Faktura", font);
         anchor.setName("Faktura");
 
         // Second parameter is the number of the chapter
         Chapter catPart = new Chapter(new Paragraph(anchor), 1);
 
-        Paragraph subPara = new Paragraph("Klient", subFont);
+        Paragraph subPara = new Paragraph("Klient", font);
         Section subCatPart = catPart.addSection(subPara);
-        subCatPart.add(new Paragraph(customer.getFirst_name()));
-        subCatPart.add(new Paragraph(customer.getLast_name()));
-        subCatPart.add(new Paragraph(customer.getEmail()));
+        subCatPart.add(new Paragraph("Imię: " + customer.getFirst_name(),font));
+        subCatPart.add(new Paragraph("Nazwisko: " +customer.getLast_name(),font));
+        subCatPart.add(new Paragraph("Email: " +customer.getEmail(), font));
 
-        subPara = new Paragraph("Telefony", subFont);
+        subPara = new Paragraph("Telefony", font);
         subCatPart = catPart.addSection(subPara);
-        subCatPart.add(new Paragraph("Paragraph 1"));
-        subCatPart.add(new Paragraph("Paragraph 2"));
-        subCatPart.add(new Paragraph("Paragraph 3"));
+
 
         // add a list
-        createList(subCatPart);
         Paragraph paragraph = new Paragraph();
         addEmptyLine(paragraph, 5);
         subCatPart.add(paragraph);
+        double cost =0;
+        for(PhoneAndQuantityInOrder item : phonesInOrder){
+            cost+= item.getPhone().getPrice() * item.getQuantity();
+        }
+        subCatPart.add(new Paragraph("Wartość: " + cost + " zł",font));
+        subCatPart.add(Chunk.NEWLINE);
 
         // add a table
         createTable(subCatPart);
 
         // now add all this to the document
         document.add(catPart);
-
-        // Next section
-        anchor = new Anchor("Second Chapter", catFont);
-        anchor.setName("Second Chapter");
-
-        // Second parameter is the number of the chapter
-        catPart = new Chapter(new Paragraph(anchor), 1);
-
-        subPara = new Paragraph("Subcategory", subFont);
-        subCatPart = catPart.addSection(subPara);
-        subCatPart.add(new Paragraph("This is a very important message"));
-
-        // now add all this to the document
-        document.add(catPart);
-
     }
 
     private void createTable(Section subCatPart)
@@ -114,35 +97,35 @@ public class Pdf {
         // t.setSpacing(4);
         // t.setBorderWidth(1);
 
-        PdfPCell c1 = new PdfPCell(new Phrase("Nazwa"));
+        PdfPCell c1 = new PdfPCell(new Phrase("Nazwa", font));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(c1);
 
-        c1 = new PdfPCell(new Phrase("Cena"));
+        c1 = new PdfPCell(new Phrase("Cena",font));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(c1);
 
-        c1 = new PdfPCell(new Phrase("Ilosc"));
+        c1 = new PdfPCell(new Phrase("Ilość",font));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(c1);
         table.setHeaderRows(1);
 
         for (PhoneAndQuantityInOrder item : phonesInOrder) {
             table.addCell(item.getPhone().getFullName());
-            table.addCell(String.valueOf(item.getPhone().getPrice()) + " zł");
+            table.addCell(String.valueOf(item.getPhone().getPrice()));
             table.addCell(String.valueOf(item.getQuantity()));
         }
         subCatPart.add(table);
 
     }
 
-    private static void createList(Section subCatPart) {
-        List list = new List(true, false, 10);
-        list.add(new ListItem("First point"));
-        list.add(new ListItem("Second point"));
-        list.add(new ListItem("Third point"));
-        subCatPart.add(list);
-    }
+//    private static void createList(Section subCatPart) {
+//        List list = new List(true, false, 10);
+//        list.add(new ListItem("First point"));
+//        list.add(new ListItem("Second point"));
+//        list.add(new ListItem("Third point"));
+//        subCatPart.add(list);
+//    }
 
     private static void addEmptyLine(Paragraph paragraph, int number) {
         for (int i = 0; i < number; i++) {
@@ -150,19 +133,20 @@ public class Pdf {
         }
     }
 
-    public void openPDF(String fileName){
+    public void openPDF(String filePath) {
         try {
-            File pdfFile = new File(fileName);
-            if (pdfFile.exists()) {
 
-                if (Desktop.isDesktopSupported()) {
-                    Desktop.getDesktop().open(pdfFile);
-                } else {
-                    System.out.println("Awt Desktop is not supported!");
-                }
+            if ((new File(filePath)).exists()) {
+
+                Process p = Runtime
+                        .getRuntime()
+                        .exec("rundll32 url.dll,FileProtocolHandler " + filePath);
+                p.waitFor();
 
             } else {
-                System.out.println("File is not exists!");
+
+                System.out.println("File is not exists");
+
             }
 
             System.out.println("Done");
